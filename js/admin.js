fbe51b22153cc3efbac4ec1e35430abb4825534b
atakanbalta <atakansahinbalta13@gmail.com>
2019-01-15 13:16:56 +0300

    Admin paneli yapıldı fakat düzenlenecek

---
+++ js/admin.js
@@ -0,0 +1,68 @@
+﻿
+(function ($) {
+    "use strict";
+    var mainApp = {
+
+        metisMenu: function () {
+
+          
+
+            $('#main-menu').metisMenu();
+
+        },
+
+
+        loadMenu: function () {
+
+
+            $(window).bind("load resize", function () {
+                if ($(this).width() < 768) {
+                    $('div.sidebar-collapse').addClass('collapse')
+                } else {
+                    $('div.sidebar-collapse').removeClass('collapse')
+                }
+            });
+        },
+        slide_show: function () {
+
+
+            $('#carousel-example').carousel({
+                interval: 3000 
+            })
+        },
+        reviews_fun: function () {
+         
+            $('#reviews').carousel({
+                interval: 2000 
+            })
+        },
+        wizard_fun: function () {
+          
+            $(function () {
+                $("#wizard").steps({
+                    headerTag: "h2",
+                    bodyTag: "section",
+                    transitionEffect: "slideLeft"
+                });
+            });
+            $(function () {
+                $("#wizardV").steps({
+                    headerTag: "h2",
+                    bodyTag: "section",
+                    transitionEffect: "slideLeft",
+                    stepsOrientation: "vertical"
+                });
+            });
+        },
+       
+        
+    };
+    $(document).ready(function () {
+        mainApp.metisMenu();
+        mainApp.loadMenu();
+        mainApp.slide_show();
+        mainApp.reviews_fun();
+        mainApp.wizard_fun();
+       
+    });
+}(jQuery));
