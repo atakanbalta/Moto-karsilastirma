dba53586bbdf9b61f4d67d61f15abaebb656e0ee
atakanbalta <atakansahinbalta13@gmail.com>
2019-01-22 14:14:57 +0300

    json çekimi başladı

--- js/karsilastirma.js
+++ js/karsilastirma.js
@@ -1,4 +1,28 @@
+window.onload=function(){
 function markaListele() {
+  var httpistegi = new XMLHttpRequest();
+  var adres = "https://raw.githubusercontent.com/atakanbalta/Moto-karsilastirma/master/markalar.json";
+
+
+  httpistegi.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var myArr = JSON.parse(this.responseText);
+      var marka1 = document.querySelector("#marka1");
+      myArr.forEach(function(params) {
+        marka1.innerHTML += '<option value="' + params.marka + '">'+ params.marka +'</option>';
+      });
+    }
+  };
+  httpistegi.open("GET", adres, true);
+  httpistegi.send();
+}
+
+markaListele();}
+
+
+
+/*window.onload=function(){
+  function markaListele() {
     var httpistegi = new XMLHttpRequest();
     var adres = "https://raw.githubusercontent.com/atakanbalta/Moto-karsilastirma/master/markalar.json";
   
@@ -6,14 +30,14 @@ function markaListele() {
     httpistegi.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         var myArr = JSON.parse(this.responseText);
-        var marka1 = document.querySelector("#marka1");
+        var marka2 = document.querySelector("#marka2");
         myArr.forEach(function(params) {
-          marka1.innerHTML += '<option value="' + params.marka + '">'+ params.marka +'</option>';
+          marka2.innerHTML += '<option value="' + params.marka + '">'+ params.marka +'</option>';
         });
       }
     };
     httpistegi.open("GET", adres, true);
     httpistegi.send();
   }
-
-  markaListele();
+  
+  markaListele();} */
